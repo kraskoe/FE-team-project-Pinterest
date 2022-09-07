710924dab4fd0d569ee764f13de0678df3ffba52
maria_akhramovich <ahramovich2003@gmail.com>
2022-09-07 18:28:07 +0300

    main-menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,12 @@
-import './main.scss';
+import '../scss/main.scss';
+
+import {buildMainMenu, buildDeskMenu} from '../js/main-menu.js';
+
+buildMainMenu();
+buildDeskMenu(1, 1);
+
+
 
 //импорт изображений
 // import image from './assets/image.jpg';
 // document.getElementById('root').innerHTML = `<img src=${image} />`;
-
-console.log('Hello, world!');
